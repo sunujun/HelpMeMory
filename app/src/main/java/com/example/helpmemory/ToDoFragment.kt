78fa11333eb99d8842e69b563b64286b8bd07fe2
seongjun.oh <tiger8708@gmail.com>
2022-05-26 18:41:18 +0900

    make calendar and add scroll feature

--- app/src/main/java/com/example/helpmemory/ToDoFragment.kt
+++ app/src/main/java/com/example/helpmemory/ToDoFragment.kt
@@ -1,26 +1,37 @@
 package com.example.helpmemory
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.view.children
 import com.example.helpmemory.databinding.CalendarDayLayoutBinding
+import com.example.helpmemory.databinding.CalendarHeaderLayoutBinding
 import com.example.helpmemory.databinding.FragmentToDoBinding
 import com.kizitonwose.calendarview.model.CalendarDay
+import com.kizitonwose.calendarview.model.CalendarMonth
+import com.kizitonwose.calendarview.model.DayOwner
 import com.kizitonwose.calendarview.ui.DayBinder
+import com.kizitonwose.calendarview.ui.MonthHeaderFooterBinder
 import com.kizitonwose.calendarview.ui.ViewContainer
+import com.kizitonwose.calendarview.utils.yearMonth
+import java.time.LocalDate
 import java.time.YearMonth
+import java.time.format.DateTimeFormatter
+import java.time.format.TextStyle
 import java.time.temporal.WeekFields
 import java.util.*
 
 class ToDoFragment : Fragment() {
-    lateinit var binding: FragmentToDoBinding
+    private lateinit var binding: FragmentToDoBinding
 
-    inner class DayViewContainer(view: View) : ViewContainer(view) {
-         val textView = CalendarDayLayoutBinding.bind(view).calendarDayText
-    }
-
+    private var selectedDate: LocalDate? = null
+    private val today = LocalDate.now()
+    private val monthTitleFormatter = DateTimeFormatter.ofPattern("MMMM")
+    private val selectionFormatter = DateTimeFormatter.ofPattern("d MMM yyyy")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,22 +49,124 @@ class ToDoFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // 날짜 binder 연결
-        binding.calendarView.dayBinder = object : DayBinder<DayViewContainer> {
-            // Called only when a new container is needed.
-            override fun create(view: View) = DayViewContainer(view)
 
-            // Called every time we need to reuse a container.
-            override fun bind(container: DayViewContainer, day: CalendarDay) {
-                container.textView.text = day.date.dayOfMonth.toString()
+        val daysOfWeek = daysOfWeekFromLocale()
+        binding.legendLayout.root.children.forEachIndexed { index, view ->
+            (view as TextView).apply {
+                text = daysOfWeek[index].getDisplayName(TextStyle.SHORT, Locale.ENGLISH).uppercase(Locale.ENGLISH)
+                setTextColor(Color.WHITE)
             }
         }
 
         val currentMonth = YearMonth.now()
-        val firstMonth = currentMonth.minusMonths(10)
-        val lastMonth = currentMonth.plusMonths(10)
-        val firstDayOfWeek = WeekFields.of(Locale.getDefault()).firstDayOfWeek
-        binding.calendarView.setup(firstMonth, lastMonth, firstDayOfWeek)
-        binding.calendarView.scrollToMonth(currentMonth)
+
+        binding.exThreeCalendar.apply {
+            setup(currentMonth.minusMonths(10), currentMonth.plusMonths(10), daysOfWeek.first())
+            scrollToMonth(currentMonth)
+        }
+
+        if (savedInstanceState == null) {
+            binding.exThreeCalendar.post {
+                // Show today's events initially.
+                selectDate(today)
+            }
+        }
+
+        class DayViewContainer(view: View) : ViewContainer(view) {
+            lateinit var day: CalendarDay // Will be set when this container is bound.
+            val binding = CalendarDayLayoutBinding.bind(view)
+
+            init {
+                view.setOnClickListener {
+                    if (day.owner == DayOwner.THIS_MONTH) {
+                        selectDate(day.date)
+                    }
+                }
+            }
+        }
+        binding.exThreeCalendar.dayBinder = object : DayBinder<DayViewContainer> {
+            override fun create(view: View) = DayViewContainer(view)
+            override fun bind(container: DayViewContainer, day: CalendarDay) {
+                container.day = day
+                val textView = container.binding.exThreeDayText
+//                val dotView = container.binding.exThreeDotView
+
+                textView.text = day.date.dayOfMonth.toString()
+
+                if (day.owner == DayOwner.THIS_MONTH) {
+                    textView.makeVisible()
+                    when (day.date) {
+                        today -> {
+//                            textView.setTextColor(Color.WHITE)
+                              textView.setTextColor(Color.BLACK)
+//                            textView.setBackgroundResource(R.drawable.example_3_today_bg)
+//                            dotView.makeInVisible()
+                        }
+                        selectedDate -> {
+                            textView.setTextColor(Color.BLUE)
+//                            textView.setBackgroundResource(R.drawable.example_3_selected_bg)
+//                            dotView.makeInVisible()
+                        }
+                        else -> {
+                            textView.setTextColor(Color.BLACK)
+                            textView.background = null
+//                            dotView.isVisible = events[day.date].orEmpty().isNotEmpty()
+                        }
+                    }
+                } else {
+                    textView.makeInVisible()
+//                    dotView.makeInVisible()
+                }
+            }
+        }
+
+        binding.exThreeCalendar.monthScrollListener = {
+            if (binding.exThreeCalendar.maxRowCount == 6) {
+                binding.exOneYearText.text = it.yearMonth.year.toString()
+                binding.exOneMonthText.text = monthTitleFormatter.format(it.yearMonth)
+            } else {
+                // In week mode, we show the header a bit differently.
+                // We show indices with dates from different months since
+                // dates overflow and cells in one index can belong to different
+                // months/years.
+                val firstDate = it.weekDays.first().first().date
+                val lastDate = it.weekDays.last().last().date
+                if (firstDate.yearMonth == lastDate.yearMonth) {
+                    binding.exOneYearText.text = firstDate.yearMonth.year.toString()
+                    binding.exOneMonthText.text = monthTitleFormatter.format(firstDate)
+                } else {
+                    binding.exOneMonthText.text =
+                        "${monthTitleFormatter.format(firstDate)} - ${monthTitleFormatter.format(lastDate)}"
+                    if (firstDate.year == lastDate.year) {
+                        binding.exOneYearText.text = firstDate.yearMonth.year.toString()
+                    } else {
+                        binding.exOneYearText.text = "${firstDate.yearMonth.year} - ${lastDate.yearMonth.year}"
+                    }
+                }
+            }
+
+            // Select the first day of the month when
+            // we scroll to a new month.
+            selectDate(it.yearMonth.atDay(1))
+        }
+    }
+
+    private fun selectDate(date: LocalDate) {
+        if (selectedDate != date) {
+            val oldDate = selectedDate
+            selectedDate = date
+            oldDate?.let { binding.exThreeCalendar.notifyDateChanged(it) }
+            binding.exThreeCalendar.notifyDateChanged(date)
+            updateAdapterForDate(date)
+        }
+    }
+
+    private fun updateAdapterForDate(date: LocalDate) {
+//        eventsAdapter.apply {
+//            events.clear()
+//            events.addAll(this@Example3Fragment.events[date].orEmpty())
+//            notifyDataSetChanged()
+//        }
+        binding.exThreeSelectedDateText.text = selectionFormatter.format(date)
     }
 }
